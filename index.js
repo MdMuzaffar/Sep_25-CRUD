9c6ee423b5534c8ef5e13b0b0ede1d77f2fa0418
Muzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-09-29 23:47:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.write('Hello World')
+console.log('Hello World Again')
+
+//Variables and data types
+// Javascript function and Parameters
+const name = "Ahmed";
+
+function upperCase(text) {
+    const uprCase = text.toUpperCase();
+    console.log(uprCase);
+}
+upperCase(name);
