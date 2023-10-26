ebfd6a0e3e192b5aff47e2554d64842036376912
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-10-26 23:06:25 +0530

    RxJS Subject

--- CRUD/src/app/app.component.ts
+++ CRUD/src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import {ajax} from 'rxjs/ajax'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CRUD';
 
   number = 50;
+
+  ngOnInit(){
+    const subject = new Subject();
+
+    subject.subscribe(d => console.log(d));
+    subject.subscribe(d => console.log(d));
+
+    subject.next(Math.random());
+
+    const sub = new Subject();
+    const data = ajax('https://jsonplaceholder.typicode.com/users');
+
+    // data.subscribe(d=> console.log(d));
+    // data.subscribe(d=> console.log(d));
+  
+    sub.subscribe(d=> console.log(d));
+    sub.subscribe(d=> console.log(d));
+    sub.subscribe(d=> console.log(d));
+
+    const reasult = data.subscribe(sub)
+
+  }
 }
